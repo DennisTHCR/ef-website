65aa48a33b0310610cacecb413f6ebe6f2a32bd2
DennisTHCR <121493649+DennisTHCR@users.noreply.github.com>
2025-04-22 15:27:45 +0200

    aahhhh

--- backend/src/models/teacher.model.ts
+++ backend/src/models/teacher.model.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToMany, ManyToOne } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToMany, ManyToMany, JoinTable } from 'typeorm';
+import { Subject } from './subject.model';
 import { Quote } from './quote.model';
 @Entity('teachers')
 export class Teacher {
@@ -10,6 +11,19 @@ export class Teacher {
 
   @Column()
   name: String;
+
+  @ManyToMany(() => Subject)
+  @JoinTable({
+    name: "teacher_subjects",
+    joinColumn: {
+      referencedColumnName: "id"
+    },
+    inverseJoinColumn: {
+      name: "subject_id",
+      referencedColumnName: "id"
+    }
+  })
+  subjects: Subject[]
 
   @CreateDateColumn()
   createdAt: Date;
