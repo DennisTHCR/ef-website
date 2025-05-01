a7ee4687db4193d7ea07a9ea52d00ce4edde3775
DennisTHCR <dennis.winghardtdd@gmail.com>
2025-05-01 17:07:15 +0200

    fini

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,7 +1,7 @@
 import { ConnectionOptions } from 'typeorm';
 import { User } from '../models/user.model';
 import { Card } from '../models/card.model';
-import { DealtCard } from '../models/dealt_card.model'; // Add this import
+import { DealtCard } from '../models/dealt_card.model';
 import { Battle } from '../models/battle.model';
 import { Season } from '../models/season.model';
 import { Pack } from '../models/pack.model';
@@ -10,7 +10,7 @@ import { Trade } from '../models/trade.model';
 export const dbConfig: ConnectionOptions = {
   type: 'sqlite',
   database: './db/teacher-card-game.sqlite',
-  entities: [User, Card, DealtCard, Battle, Season, Pack, Trade], // Add DealtCard here
-  synchronize: true, // Set to false in production
+  entities: [User, Card, DealtCard, Battle, Season, Pack, Trade],
+  synchronize: true,
   logging: true
 };
